34d48a1b280f3cbeb2ab3f12874adca06a07b3fb
kaiquethedog <52429335+kaiquethedog@users.noreply.github.com>
2020-12-08 12:40:12 -0300

    Listagem de Eventos

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,13 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button, Image, FlatList } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import ItemEvento from '../../components/index';
 
 
 const Home = () => {
 
-    <View style={styles.container}>
-    <Text>Bem Vindo ao meu App</Text>
-	</View>
+    const [token, setToken] = useState('');
+    const [eventos, setEventos] = useState([]);
+
+
+    const getToken = async () => {
+    setToken(await AsyncStorage.getItem('@jwt'));
+    }
+
+      
+    // useEffect(()=>{
+    //     getToken();
+
+    // }, [])
+
+    //   useEffect(() => {
+    //       listarEventos();
+    //   }, []); 
+
+    const listarEventos = () => {
+        fetch('http://192.168.4.117:5000/api/Eventos')
+            .then(response => response.json())
+            .then(data => {
+                setEventos(data.data)
+                console.log(data.data);
+            })
+            .catch(err => console.error(err));
+    }
+
+    const renderItem = ({ item }) => (
+        <ItemEvento nome={item.nome} imagem={item.urlImagem} link={item.link} />
+      );
+
+    return(
+        <View>
+            <Text>HOME</Text>
+            {/* <Text>{token}</Text> */}
+            <FlatList
+                data={eventos}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
+        </View>
+    )
 }
 
 export default Home; 
